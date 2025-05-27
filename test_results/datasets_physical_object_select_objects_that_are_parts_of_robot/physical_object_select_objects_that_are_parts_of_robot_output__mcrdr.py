12b9914755c9c86ed1cdb222c91ce393f991968f
Abdelrhman Bassiouny <abdelrhman_bassuny@hotmail.com>
2025-05-27 12:34:11 +0200

    [RDRViews] fixed updates from loaded imported from edit file template.

--- test_results/datasets_physical_object_select_objects_that_are_parts_of_robot/physical_object_select_objects_that_are_parts_of_robot_output__mcrdr.py
+++ test_results/datasets_physical_object_select_objects_that_are_parts_of_robot/physical_object_select_objects_that_are_parts_of_robot_output__mcrdr.py
@@ -6,7 +6,7 @@ from ripple_down_rules.rdr import MultiClassRDR
 
 
 attribute_name = 'output_'
-conclusion_type = (set, PhysicalObject, list,)
+conclusion_type = (set, list, PhysicalObject,)
 type_ = MultiClassRDR
 
 
--- test_results/datasets_physical_object_select_objects_that_are_parts_of_robot/physical_object_select_objects_that_are_parts_of_robot_output__mcrdr_defs.py
+++ test_results/datasets_physical_object_select_objects_that_are_parts_of_robot/physical_object_select_objects_that_are_parts_of_robot_output__mcrdr_defs.py
@@ -1,8 +1,8 @@
-from datasets import Robot
-from typing import Dict
-from datasets import PhysicalObject
 from typing import List
+from datasets import Robot
+from datasets import PhysicalObject
 from typing import Union
+from typing import Dict
 from typing_extensions import Union
 
 
