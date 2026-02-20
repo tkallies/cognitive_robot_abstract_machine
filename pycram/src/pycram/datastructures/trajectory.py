c62aa451de05195edd90fe26f345e38cce807908
sunava <hassouna@uni-bremen.de>
2026-02-20 10:06:28 +0100

    changed to post_init

--- pycram/src/pycram/datastructures/trajectory.py
+++ pycram/src/pycram/datastructures/trajectory.py
@@ -7,7 +7,7 @@ from typing_extensions import Iterable, Tuple
 from .pose import PoseStamped
 
 
-@dataclass(frozen=True)
+@dataclass()
 class PoseTrajectory:
     """
     Immutable wrapper for a sequence of waypoint poses.
@@ -18,10 +18,5 @@ class PoseTrajectory:
     Ordered TCP waypoints.
     """
 
-    def __init__(self, poses: Iterable[PoseStamped]):
-        poses_tuple = tuple(poses)
-        if not poses_tuple:
-            raise ValueError("Provide at least one PoseStamped.")
-        if not all(isinstance(pose, PoseStamped) for pose in poses_tuple):
-            raise ValueError("All poses  must be of type PoseStamped.")
-        object.__setattr__(self, "poses", poses_tuple)
+    def __post_init__(self):
+        self.poses = tuple(self.poses)
